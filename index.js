42573750cf3c584d2f99810abc95a71f596342e2
Taylor Romero <me@taylorrome.ro>
2024-06-24 12:38:25 -0600

    patch: no more Q

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict'
-const Q = require(`q`)
 const conventionalChangelog = require(`./conventional-changelog`)
 const parserOpts = require(`./parser-opts`)
 const recommendedBumpOpts = require(`./conventional-recommended-bump`)
 const writerOpts = require(`./writer-opts`)
 
 module.exports = function () {
-  return Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
-    .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
+  return Promise.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
+    .then(([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts]) => {
       return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts }
     })
 }
