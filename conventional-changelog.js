25f9d4b933111decd0d590645e84eef456b0cd09
Taylor Romero <me@taylorrome.ro>
2024-06-24 13:50:12 -0600

    patch: changelog test

--- conventional-changelog.js
+++ conventional-changelog.js
@@ -3,7 +3,4 @@
 const parserOpts = require(`./parser-opts`)
 const writerOpts = require(`./writer-opts`)
 
-module.exports = Promise.all([parserOpts, writerOpts])
-  .then(([parserOpts, writerOpts]) => {
-    return { parserOpts, writerOpts }
-  })
+module.exports = { parserOpts, writerOpts }
--- index.js
+++ index.js
@@ -5,8 +5,5 @@ const recommendedBumpOpts = require(`./conventional-recommended-bump`)
 const writerOpts = require(`./writer-opts`)
 
 module.exports = function () {
-  return Promise.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
-    .then(([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts]) => {
-      return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts }
-    })
+  return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts }
 }
